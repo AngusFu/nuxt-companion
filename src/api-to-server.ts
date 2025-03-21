a6bc21498a005cdae83bfc8d9af6f027bd022b50
AngusFu <angusfu1126@qq.com>
2025-03-21 10:03:29 +0800

    feat: 支持  definition redirect

---
+++ src/api-to-server.ts
@@ -0,0 +1,178 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+import { parseSync } from "@oxc-parser/wasm";
+import * as t from "@oxc-project/types";
+import * as esquery from "esquery";
+import { debounce } from "lodash-es";
+
+const eQuery = (node: t.Span, selector: string) =>
+  esquery.query(node as any, selector);
+const SUPPORTED_LANGUAGES = ["typescript", "typescriptreact"];
+
+const astCache = new Map<string, t.CallExpression[]>();
+const buildASTCache = (document: vscode.TextDocument) => {
+  const documentContent = document.getText(
+    new vscode.Range(
+      new vscode.Position(0, 0),
+      new vscode.Position(
+        document.lineCount,
+        document.lineAt(document.lineCount - 1).text.length
+      )
+    )
+  );
+  const parsed = parseSync(documentContent, {
+    sourceFilename: document.uri.path,
+  });
+  const callExprs = [
+    ...eQuery(
+      parsed.program as unknown as any,
+      'CallExpression[callee.name="$api"]'
+    ),
+    ...eQuery(
+      parsed.program as unknown as any,
+      'CallExpression[callee.property.name="$api"]'
+    ),
+  ];
+
+  return callExprs as t.CallExpression[];
+};
+
+function getDocumentAPICalls(document: vscode.TextDocument) {
+  let callExpressions = astCache.get(document.uri.path);
+  if (!callExpressions?.length) {
+    const ast = buildASTCache(document);
+    astCache.set(document.uri.path, ast);
+    callExpressions = ast;
+  }
+  return callExpressions;
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  const onDocumentChange = debounce(
+    (document: vscode.TextDocument) => {
+      astCache.delete(document.uri.path);
+      astCache.set(document.uri.path, buildASTCache(document));
+    },
+    1000,
+    {
+      leading: true,
+      trailing: true,
+    }
+  );
+
+  context.subscriptions.push(
+    vscode.languages.registerDefinitionProvider(
+      SUPPORTED_LANGUAGES,
+      new CustomDefinitionProvider()
+    ),
+
+    // 文档关闭
+    vscode.workspace.onDidCloseTextDocument((document) => {
+      astCache.delete(document.uri.path);
+    }),
+    // 监听文档内容变化
+    vscode.workspace.onDidChangeTextDocument(async ({ document }) => {
+      if (
+        SUPPORTED_LANGUAGES.includes(document.languageId) ||
+        /\.tsx?$/.test(document.uri.path)
+      ) {
+        onDocumentChange(document);
+      }
+    })
+  );
+}
+
+class CustomDefinitionProvider implements vscode.DefinitionProvider {
+  async provideDefinition(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    cancelToken: vscode.CancellationToken
+  ) {
+    const folder = vscode.workspace.getWorkspaceFolder(document.uri)?.uri;
+    if (!folder) return null;
+
+    const serverFolder = vscode.Uri.joinPath(folder, "server");
+    if (document.uri.fsPath.startsWith(serverFolder.fsPath)) return null;
+
+    // 插值情况忽略
+    if (document.getWordRangeAtPosition(position, /\$\{\s*\w+\s*\}/)) {
+      return null;
+    }
+
+    const quotedRange = document.getWordRangeAtPosition(
+      position,
+      /((['"])(?:(?!\2).)*\2|`(?:[^`\\]|\\.)*`)/
+    );
+    if (!quotedRange) return null;
+
+    const callExpressions = getDocumentAPICalls(document);
+    const expr = (callExpressions || []).find((el) => {
+      const range = new vscode.Range(
+        document.positionAt(el.start),
+        document.positionAt(el.end)
+      );
+      return range.contains(quotedRange);
+    });
+    if (!expr?.arguments.length) return null;
+
+    const path = expr.arguments[0];
+    const config = expr.arguments[1];
+
+    let method = "get";
+    if (config && config.type === "ObjectExpression") {
+      const res = eQuery(
+        config,
+        'Property[key.name="method"]>Literal'
+      )?.[0] as t.StringLiteral;
+      method = res?.value.toLowerCase() || "get";
+    }
+
+    let glob = "";
+    let filter = (uri: vscode.Uri) => !!uri;
+    if (path.type === "Literal" && typeof path.value === "string") {
+      glob = path.value.replace(/^\//, "");
+    } else if (path.type === "TemplateLiteral") {
+      glob = path.quasis
+        .map((el) => el.value.raw)
+        .join("*")
+        .replace(/^\//, "");
+      const regex = RegExp(glob.replace(/\*/g, () => "\\[.+\\]"));
+      filter = (uri: vscode.Uri) => regex.test(uri.path);
+    }
+    if (!glob) return null;
+
+    if (cancelToken.isCancellationRequested) return null;
+    const files = (
+      await Promise.all(
+        [`${glob}.${method}.ts`, `${glob}/index.${method}.ts`].map((pattern) =>
+          vscode.workspace.findFiles(
+            new vscode.RelativePattern(serverFolder, pattern)
+          )
+        )
+      )
+    )
+      .flat()
+      .filter(filter);
+    if (cancelToken.isCancellationRequested) return null;
+
+    if (files[0]) {
+      const targetRange = new vscode.Range(
+        new vscode.Position(0, 0),
+        new vscode.Position(0, 0)
+      );
+      const targetLoc = new vscode.Location(files[0], targetRange);
+      const locationLinks: vscode.LocationLink[] = [
+        {
+          targetUri: targetLoc.uri,
+          targetRange: targetRange,
+          originSelectionRange: quotedRange,
+        },
+      ];
+
+      return locationLinks;
+    }
+
+    return null;
+  }
+}
---
+++ src/go-to-alias.ts
@@ -0,0 +1,179 @@
+/**
+ * original code from https://github.com/antfu/vscode-goto-alias
+ */
+import { parseSync } from "@oxc-parser/wasm";
+import * as vscode from "vscode";
+import * as esquery from "esquery";
+import * as t from "@oxc-project/types";
+
+const eQuery = (node: t.Span, selector: string) =>
+  esquery.query(node as any, selector);
+
+function showSettingsUpdateDialog(ext: vscode.ExtensionContext) {
+  if (
+    vscode.workspace
+      .getConfiguration()
+      .get("editor.gotoLocation.multipleDefinitions") === "goto"
+  )
+    return;
+
+  if (ext.globalState.get("showedSettingsUpdateDialog")) return;
+
+  vscode.window
+    .showInformationMessage(
+      [
+        "[Goto Alias]",
+        "To get the best experience, we recommend you to set",
+        '`"editor.gotoLocation.multipleDefinitions": "goto"` to the first definition automatically.',
+        'Click "OK" to set it now.',
+      ].join("\n"),
+      "OK",
+      "Not now"
+    )
+    .then((selection) => {
+      if (selection === "OK") {
+        // open user settings json
+        vscode.commands.executeCommand("workbench.action.openSettingsJson");
+        vscode.workspace
+          .getConfiguration()
+          .update("editor.gotoLocation.multipleDefinitions", "goto", true);
+      }
+      ext.globalState.update("showedSettingsUpdateDialog", true);
+    });
+}
+function getRangeFromOffset(
+  document: vscode.TextDocument,
+  baseRange: vscode.Range,
+  startOffset: number,
+  endOffset: number
+): vscode.Range {
+  // 获取基础范围的起始偏移量
+  const baseStartOffset = document.offsetAt(baseRange.start);
+
+  // 计算实际的起始和结束偏移量
+  const actualStartOffset = baseStartOffset + startOffset;
+  const actualEndOffset = baseStartOffset + endOffset;
+
+  // 将偏移量转换回位置
+  const startPosition = document.positionAt(actualStartOffset);
+  const endPosition = document.positionAt(actualEndOffset);
+
+  return new vscode.Range(startPosition, endPosition);
+}
+
+async function provideDefinition(
+  document: vscode.TextDocument,
+  position: vscode.Position
+) {
+  const definitions = await vscode.commands.executeCommand(
+    "vscode.executeDefinitionProvider",
+    document.uri,
+    position
+  );
+  if (!Array.isArray(definitions) || !definitions.length) {
+    return definitions as vscode.DefinitionLink[];
+  }
+
+  const modifiedDefinitions = [] as vscode.DefinitionLink[];
+  for (const definition of definitions) {
+    if (!("targetUri" in definition)) {
+      modifiedDefinitions.push(definition);
+      continue;
+    }
+
+    const { originSelectionRange, targetUri, targetRange } =
+      definition as vscode.DefinitionLink;
+
+    if (targetUri.fsPath.endsWith(".d.ts")) {
+      const doc = await vscode.workspace.openTextDocument(targetUri);
+      const content = doc.getText(targetRange);
+      let contentRange = targetRange;
+
+      // avoid ts variable declarations
+      const replaced = content.replace(/^\s*(?:let|const|var)\s/, "");
+      if (replaced !== content) {
+        const startOffset = content.length - replaced.length;
+        const endOffset = startOffset + replaced.length;
+        contentRange = getRangeFromOffset(
+          doc,
+          targetRange,
+          startOffset,
+          endOffset
+        );
+      }
+
+      const parsed = parseSync(replaced, {
+        sourceFilename: targetUri.path,
+      });
+      const imports = eQuery(
+        parsed.program as unknown as any,
+        "ImportExpression"
+      );
+      const labels = eQuery(
+        parsed.program as unknown as any,
+        "LabeledStatement"
+      );
+      const members = eQuery(
+        parsed.program as unknown as any,
+        "MemberExpression > Literal"
+      );
+
+      if (!labels.length || !imports.length || !members.length) {
+        modifiedDefinitions.push(definition);
+        continue;
+      }
+      const m = members[0] as t.StringLiteral;
+      const range = getRangeFromOffset(doc, contentRange, m.start, m.end);
+      const dtsDefinitions: vscode.DefinitionLink[] =
+        await vscode.commands.executeCommand(
+          "vscode.executeDefinitionProvider",
+          targetUri,
+          range.start
+        );
+
+      if (dtsDefinitions.length) {
+        // unshift to keep this definition as primary
+        // when set `"editor.gotoLocation.multipleDefinitions": "goto"`, it will go to the right file
+        const links = dtsDefinitions.map(
+          (el) =>
+            ({
+              ...el,
+              originSelectionRange,
+            } as vscode.DefinitionLink)
+        );
+        modifiedDefinitions.unshift(...links);
+      } else {
+        modifiedDefinitions.push(definition);
+      }
+    }
+  }
+
+  return modifiedDefinitions;
+}
+
+export function activate(ext: vscode.ExtensionContext) {
+  let lock = false;
+  showSettingsUpdateDialog(ext);
+
+  vscode.languages.registerDefinitionProvider(
+    ["typescript", "typescriptreact", "vue"],
+    {
+      async provideDefinition(
+        document: vscode.TextDocument,
+        position: vscode.Position,
+        cancelToken: vscode.CancellationToken
+      ) {
+        // prevent infinite loop and reduce unnecessary calls
+        if (lock) return null;
+        try {
+          lock = true;
+          const res = await provideDefinition(document, position);
+          if (cancelToken.isCancellationRequested) return null;
+          return res;
+        } finally {
+          lock = false;
+        }
+      },
+    }
+  );
+}
