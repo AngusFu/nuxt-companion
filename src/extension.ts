3a2e20b33440a698ee1a4a98448837a4577aa847
wemlion <angusfu1126@qq.com>
2025-03-31 13:21:32 +0800

    feat: layouts name intelligence

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,24 @@ import * as vscode from "vscode";
 
 import { activate as apiToServer } from "./api-to-server";
 import { activate as goToAliasActivate } from "./go-to-alias";
+import { activate as layoutsNameIntelligence } from "./layouts-name";
 
 export function activate(context: vscode.ExtensionContext) {
-  apiToServer(context);
-  goToAliasActivate(context);
+  const config = vscode.workspace.getConfiguration("nuxtCompanion");
+
+  if (config.get<boolean>("enableApiToServer")) {
+    apiToServer(context);
+  }
+
+  if (config.get<boolean>("enableGoToAlias")) {
+    goToAliasActivate(context);
+  }
+
+  if (config.get<boolean>("enableLayoutsNameIntelligence")) {
+    layoutsNameIntelligence(context);
+  }
+}
+
+export function deactivate() {
+  // nothing to do
 }
---
+++ src/layouts-name.ts
@@ -0,0 +1,158 @@
+import * as vscode from "vscode";
+import { basename, dirname, extname, normalize, resolve } from "pathe";
+import { kebabCase, splitByCase } from "scule";
+import { withTrailingSlash } from "ufo";
+import { globby } from "globby";
+
+const QUOTE_RE = /["']/g;
+function getNameFromPath(path: string, relativeTo?: string) {
+  const relativePath = relativeTo
+    ? normalize(path).replace(withTrailingSlash(normalize(relativeTo)), "")
+    : basename(path);
+  const prefixParts = splitByCase(dirname(relativePath));
+  const fileName = basename(relativePath, extname(relativePath));
+  const segments = resolveComponentNameSegments(
+    fileName.toLowerCase() === "index" ? "" : fileName,
+    prefixParts
+  ).filter(Boolean);
+  return kebabCase(segments).replace(QUOTE_RE, "");
+}
+
+function resolveComponentNameSegments(fileName: string, prefixParts: string[]) {
+  /**
+   * Array of fileName parts split by case, / or -
+   * @example third-component -> ['third', 'component']
+   * @example AwesomeComponent -> ['Awesome', 'Component']
+   */
+  const fileNameParts = splitByCase(fileName);
+  const fileNamePartsContent = fileNameParts.join("/").toLowerCase();
+  const componentNameParts: string[] = prefixParts.flatMap((p) =>
+    splitByCase(p)
+  );
+  let index = prefixParts.length - 1;
+  const matchedSuffix: string[] = [];
+  while (index >= 0) {
+    const prefixPart = prefixParts[index]!;
+    matchedSuffix.unshift(
+      ...splitByCase(prefixPart).map((p) => p.toLowerCase())
+    );
+    const matchedSuffixContent = matchedSuffix.join("/");
+    if (
+      fileNamePartsContent === matchedSuffixContent ||
+      fileNamePartsContent.startsWith(matchedSuffixContent + "/") ||
+      // e.g Item/Item/Item.vue -> Item
+      (prefixPart.toLowerCase() === fileNamePartsContent &&
+        prefixParts[index + 1] &&
+        prefixParts[index] === prefixParts[index + 1])
+    ) {
+      componentNameParts.length = index;
+    }
+    index--;
+  }
+  return [...componentNameParts, ...fileNameParts];
+}
+
+// https://github.com/nuxt/nuxt/blob/2a1e192bced0e85faa2dff93df5f4910799e80b6/packages/nuxt/src/core/app.ts#L158
+async function getLayoutsInfo() {
+  const layouts: Record<string, { name: string; file: string }> = {};
+
+  const srcDir = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath;
+  if (!srcDir) {
+    return layouts;
+  }
+
+  const layoutDir = "layouts";
+  const layoutFiles = await globby(`${layoutDir}/**/*.vue`, {
+    cwd: srcDir,
+    absolute: true,
+  });
+
+  for (const file of layoutFiles) {
+    const name = getNameFromPath(file, resolve(srcDir, layoutDir));
+
+    layouts[name] ||= { name, file };
+  }
+
+  return layouts;
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  const workspaceFolder = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath;
+  if (!workspaceFolder) return;
+
+
+  // watch layouts dir and update layouts info
+  // create fs watcher
+  const watcher = vscode.workspace.createFileSystemWatcher(
+    new vscode.RelativePattern(workspaceFolder, 'layouts/**/*.vue')
+  );
+  let layoutsInfo: Record<string, { name: string; file: string }> = {};
+  watcher.onDidCreate(async (e) => {
+    layoutsInfo = await getLayoutsInfo();
+  });
+  watcher.onDidChange(async (e) => {
+    layoutsInfo = await getLayoutsInfo();
+  });
+  watcher.onDidDelete(async (e) => {
+    layoutsInfo = await getLayoutsInfo();
+  });
+  getLayoutsInfo().then((layouts) => {
+    layoutsInfo = layouts;
+  });
+
+  context.subscriptions.push(watcher);
+
+  vscode.languages.registerDefinitionProvider(["vue"], {
+    provideDefinition: async (document, pos) => {
+      const quotedRange = document.getWordRangeAtPosition(
+        pos,
+        /['"][\w-]+['"]/
+      );
+      if (!quotedRange) return null;
+
+      const attrRange = document.getWordRangeAtPosition(
+        pos,
+        /name=['"][\w-]+['"]/
+      );
+      if (!attrRange) return null;
+
+      // check closets `<NuxtLayout`
+      const text = document
+        .getText()
+        .slice(0, document.offsetAt(attrRange.start));
+      let nearestNuxtLayout = text.lastIndexOf("<NuxtLayout");
+      if (nearestNuxtLayout < 0) {
+        nearestNuxtLayout = text.lastIndexOf("<nuxt-layout");
+        if (nearestNuxtLayout < 0) {
+          return null;
+        }
+      }
+
+      const nearestOpenTag = text.lastIndexOf("<");
+      const nearestCloseTag = text.lastIndexOf(">");
+      if (
+        nearestCloseTag < nearestNuxtLayout &&
+        nearestOpenTag === nearestNuxtLayout
+      ) {
+        const name = document.getText(quotedRange).slice(1, -1);
+        const layout = layoutsInfo[name];
+        if (layout) {
+          const targetLoc = new vscode.Location(vscode.Uri.file(layout.file), new vscode.Position(0, 0));
+          const locationLinks: vscode.LocationLink[] = [
+            {
+              targetUri: targetLoc.uri,
+              targetRange: new vscode.Range(
+                new vscode.Position(0, 0),
+                new vscode.Position(0, 0)
+              ),
+              originSelectionRange: quotedRange,
+            },
+          ];
+
+          return locationLinks;
+        }
+      }
+      return null;
+    },
+  });
+}
